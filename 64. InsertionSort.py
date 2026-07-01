1b9c08ca10e2942f89a3f44aa2f938b1e453df68
Savvana Lohitha <lohithasavvana@gmail.com>
2026-07-01 18:17:06 +0530

    Implement Insertion Sort algorithm

---
+++ 64. InsertionSort.py
@@ -0,0 +1,12 @@
+print("Enter List:")
+a = list(map(int, input().split()))
+n = len(a)
+for i in range(1, n):
+    key = a[i]
+    j = i - 1
+    while j >= 0 and a[j] > key:
+        a[j + 1] = a[j]
+        j -= 1
+    a[j + 1] = key
+print("Sorted List:")
+print(a)
